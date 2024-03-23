ef6e947257720bfe25dbc0254bc0b6f513f6902e
Arielle Fox <118761163+ArielleFox@users.noreply.github.com>
2024-03-23 16:39:05 +0100

    Create lmao.sh
    Proof-Of-Concept Algorithm Created

---
+++ lmao.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env
+echo '###################################'
+echo '- This algorithm bootstraps my    -'
+echo '- tools by injecting data into ~/ -'
+echo '- and loads them into environment.-'
+echo '- ➤  Arielle Steinlechner 2024    -'
+echo '###################################' && clear
+# PREPARING
+chmod +x *.sh
+unlink ~/.inject/bin/task
+sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d -b ~/.inject/bin
+# SYMBOLIC LINKING
+# LOADING
+alias l='ls'
+alias lsa='ls -a'
+alias lmao='bash .injectHomedir.sh'
+alias task='~/.inject/bin/task'
+unalias lmao
